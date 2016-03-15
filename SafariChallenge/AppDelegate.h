ad85a64572c5eb639f28812140397b0ccbf67ca6
dp <dp@dp.local>
2016-03-15 14:12:56 -0500

    Initial Commit

---
+++ SafariChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariChallenge
+//
+//  Created by dp on 3/15/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SafariChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafariChallenge
+//
+//  Created by dp on 3/15/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
